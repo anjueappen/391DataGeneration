30251443598cf171ac1f814d9e8dec2ef2c9c90e
Anju Eappen <aeappen@ualberta.ca>
2016-03-20 14:15:36 -0600

    transformer changes

--- src/main/java/MachineTransformer.java
+++ src/main/java/MachineTransformer.java
@@ -20,17 +20,16 @@ public class MachineTransformer implements DataTransformer {
      * @param cr a reference to DataPipe from which to read the current map
      */
     public void transform(DataPipe cr) {
+
         for (Map.Entry<String, String> entry : cr.getDataMap().entrySet()) {
             String value = entry.getValue();
 
             if (value.equals("#{random_string}")) {
                 entry.setValue(randomStringUtils.randomAlphabetic(10).toUpperCase());
-            }else if (value.equals("#{b}")){
-                entry.setValue("replaced by Anju");
-            }else if (value.equals("#{c}")){
-                entry.setValue("another");
             }else if (value.equals("#{random_integer}")){
                 entry.setValue(Integer.toString(rand.nextInt()));
+            }else if (value.equals("#{random_decimal}")){
+                entry.setValue(Double.toString(rand.nextDouble()));
             }
         }
     }
